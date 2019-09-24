a41b8ce28ef1b276f344b28130834a8a79895341
paulzfm <zhufengminpaul@163.com>
2019-09-24 21:59:49 +0800

    test on maven local

--- src/main/groovy/decaf/tools/gradle/plugin/ll1pg/LL1pgPlugin.groovy
+++ src/main/groovy/decaf/tools/gradle/plugin/ll1pg/LL1pgPlugin.groovy
@@ -1,15 +1,15 @@
 package decaf.tools.gradle.plugin.ll1pg
 
-import decaf.tools.gradle.task.LL1ParserGenTask
+import decaf.tools.gradle.task.LL1pgTask
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 
-class LL1ParserGenPlugin implements Plugin<Project> {
+class LL1pgPlugin implements Plugin<Project> {
     @Override
     void apply(Project project) {
         project.with {
             apply plugin: 'java'
-            tasks.create(name: 'll1pg', type: LL1ParserGenTask)
+            tasks.create(name: 'll1pg', type: LL1pgTask)
             tasks.compileJava.dependsOn tasks.ll1pg
             sourceSets.main.java.srcDirs += tasks.ll1pg.generateDir
         }
--- src/main/groovy/decaf/tools/gradle/task/LL1pgTask.groovy
+++ src/main/groovy/decaf/tools/gradle/task/LL1pgTask.groovy
@@ -1,17 +1,19 @@
 package decaf.tools.gradle.task
 
+import decaf.tools.ll1pg.Main
 import org.gradle.api.DefaultTask
+import org.gradle.api.file.ConfigurableFileTree
 import org.gradle.api.file.FileVisitDetails
 import org.gradle.api.tasks.InputDirectory
 import org.gradle.api.tasks.Optional
 import org.gradle.api.tasks.OutputDirectory
 import org.gradle.api.tasks.TaskAction
 
-class LL1ParserGenTask extends DefaultTask {
+class LL1pgTask extends DefaultTask {
 
     @InputDirectory
     @Optional
-    File source = project.file('src/main/ll1pg')
+    File sourceDir = project.file('src/main/ll1pg')
 
     @OutputDirectory
     @Optional
@@ -25,11 +27,16 @@ class LL1ParserGenTask extends DefaultTask {
     }
 
     private void generate() {
-        project.fileTree(dir: source, include: '**/*.spec').visit { FileVisitDetails file ->
+        ConfigurableFileTree files = project.fileTree(dir: sourceDir, include: '**/*.spec')
+        if (files.empty) System.err.println("No *.spec file found in src/main/ll1pg/, skip")
+        files.visit { FileVisitDetails file ->
             if (file.isDirectory()) {
+                System.err.println("Skip directory: " + file.file.path)
                 return
             }
-            // Call LL1-parser-gen
+
+            // Call ll1pg
+            Main.main(file.file.path, generateDir.path)
         }
     }
 }
